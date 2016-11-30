4a34f265264d04df48c56a0823d4eb1403c20479
a397996616 <a397996616@163.com>
2016-11-30 12:46:57 -0500

    Update credit.c

--- credit.c
+++ credit.c
@@ -7,16 +7,16 @@ int main(void)
 {
     long long cc_number;
     int length = 0;
-    char cc_string[17];
+    char cc_string[19];
     int sum =0;
     int sum2 =0;
     int sum3 =0;
-    printf("credit card number: "); //ask user for cc number
-    cc_number = get_long_long(); //get cc number
+    printf("credit card number: "); //ask user for credit card number
+    cc_number = get_long_long(); //get credit card number save as long
     sprintf(cc_string, "%llu", cc_number); //convert long to string
-    length = strlen(cc_string); //get cc number length
+    length = strlen(cc_string); //get credit card number length
     
-    for(int i= (length-2); i >= 0; i-=2)//get sum of 2 times every other digit
+    for(int i= (length-2); i >= 0; i-=2)//get sum of 2 times every other digit from second last number
     {
     	int d;
     	int s;
@@ -35,8 +35,6 @@ int main(void)
     
     sum3 = sum + sum2;//add 2 sums together
 	
-	printf("sum = %i\n", sum3);//check if the number right, only use in debugging.
-	
 	if(sum3 % 10 == 0)// check if the card number valid
 	{
 		printf("credit card VALID\n");
@@ -45,7 +43,7 @@ int main(void)
 	    	printf("credit card belong to American Express\n");     
     	}
     	
-    	else if((cc_string[0]-48) == 4)                             // VISA
+    	else if((cc_string[0]-48) == 4)// VISA
     	{
 		    printf("credit card belong to VISA\n");
     	}
